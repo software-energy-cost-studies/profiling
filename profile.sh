357bb712c4b6e1ea356d1975b0e34145b27645fa
ldidukh <Leonid.Didukh@thras.io>
2023-07-12 14:03:55 +0300

    added code carbon and impact tracker dependencies for energy estimation to .toml

--- profile.sh
+++ profile.sh
@@ -1,18 +1,18 @@
 git clone https://github.com/baler-collaboration/baler.git
 
 #cProfile
-python3 -m cProfile -o cProfile_train.prof  baler/baler.py --mode train --project profile cpu
-python3 -m cProfile -o cProfile_compress.prof  baler/baler.py --mode compress --project profile cpu
-python3 -m cProfile -o cProfile_decompress.prof  baler/baler.py --mode decompress --project profile cpu
+poetry run python3 -m cProfile -o cProfile_train.prof  baler/baler.py --mode train --project profile cpu
+poetry run python3 -m cProfile -o cProfile_compress.prof  baler/baler.py --mode compress --project profile cpu
+poetry run python3 -m cProfile -o cProfile_decompress.prof  baler/baler.py --mode decompress --project profile cpu
 
 #scalene
 #the training profiling using scalene doesn't work
 #scalene --cpu --gpu --memory baler/baler.py --mode train --project profile cpu
-scalene --cpu --gpu --memory baler/baler.py --mode compress --project profile cpu
-scalene --cpu --gpu --memory baler/baler.py --mode decompress --project profile cpu
+poetry run scalene --cpu --gpu --memory baler/baler.py --mode compress --project profile cpu
+poetry run scalene --cpu --gpu --memory baler/baler.py --mode decompress --project profile cpu
 
 
 #py-spy
-py-spy top -- python3 baler/baler.py --mode train --project profile cpu
-py-spy top -- python3 baler/baler.py --mode compress --project profile cpu
-py-spy top -- python3 baler/baler.py --mode decompress --project profile cpu
+poetry run py-spy top -- python3 baler/baler.py --mode train --project profile cpu
+poetry run py-spy top -- python3 baler/baler.py --mode compress --project profile cpu
+poetry run py-spy top -- python3 baler/baler.py --mode decompress --project profile cpu
